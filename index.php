38eef11a5911effcb520091c5d876a2cffac1724
git-tomas <gootomas@gmail.com>
2015-11-20 19:20:37 +0100

    Estructura de la Base de Datos, Actualización de notas, grupos de trabajo y de control de versiones.

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+//header('HTTP/1.1 301 Moved Permanently',true);
+header('HTTP/1.1 302 Moved Temporaly',true);
+//header('Location: cake/webroot/',true);
+header('Location: cake/',true);
+exit;
