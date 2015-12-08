5cc238d7a772480b97dc9d1b391cb1d1fc35394c
git-tomas <gootomas@gmail.com>
2015-12-08 21:20:17 +0100

    Modificaciones en gestión de Usuarios y control de Autorizaciones.

--- sql_2015-12-08-01 actualizar campo password.sql
+++ sql_2015-12-08-01 actualizar campo password.sql
@@ -5,4 +5,7 @@
 -- del componente "Auth" en el atributo "passwordHasher", sino debe tener
 -- entre 60 y 255 caracteres si se usa el tipo "PASSWORD_DEFAULT" que es
 -- el predeterminado.
-ALTER TABLE `usuarios` CHANGE `password` `password` VARCHAR( 60 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
+-- El usuario que se crea con ID=1 tiene de clave "admin".
+ALTER TABLE `daw_recetas`.`usuarios` CHANGE `password` `password` VARCHAR( 60 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
+
+INSERT INTO `daw_recetas`.`usuarios` (`id`, `email`, `password`, `nombre`, `rol`, `aceptado`, `creado`) VALUES ('1', 'admin@admin.es', '$2y$10$LCEFSOiRqUrVLET3macDQ.3ZwYmtMpQtqFWR.Oz56w6hLu1T6kHKS', 'Admin', 'A', '1', '2015-12-08 20:07:00');
