42b06da84e3cf244f93bc9a54f2fe85e8411f220
OliverUronesGarcia <Oliver.Urones@usal.es>
2016-01-09 13:07:09 +0100

    Update AppController.php

--- cake/src/Controller/AppController.php
+++ cake/src/Controller/AppController.php
@@ -113,7 +113,7 @@ class AppController extends Controller
         
         //DTR: Por defecto los usuarios registrados de "rol" administrador 
         //pueden acceder a cualquier accion.
-        if (!$res && (strcasecmp( $rol, 'A') == 0)) {
+        if (!$res && (strcasecmp( $rol, 'A') == 0) && $accion != 'cambiarRol' && $accion != 'hacerBackup' /*Para el caso de que sólo el sysadmin pueda hacer copias de seguridad*/) {
           $res= true;
         }
         //DTR: Por ejemplo, si se quiere comprobar una accion en concreto en
