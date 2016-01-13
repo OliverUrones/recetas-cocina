e3e054b55b800c6c8c2988a02b8d2daacaa17c40
cmonherusal <cmonher19901@gmail.com>
2016-01-13 09:41:52 +0100

    cambios importantes en categorias, traducciones, subcategorias, selects...

--- cake/src/Controller/CategoriasController.php
+++ cake/src/Controller/CategoriasController.php
@@ -32,14 +32,12 @@ class CategoriasController extends AppController
     public function view($id = null)
     {
         $categoria = $this->Categorias->get($id);
+		$categoria->categorias = $this->paginate($this->Categorias);
 		$nombre_padre="";
 		if($categoria->categoria_id!=0){
 		 $padre = $this->Categorias->get($categoria->categoria_id);
 		 $nombre_padre=$padre->nombre;
 		}
-		
-	$hijos = $this->Categorias->find('hijos', ['padre' => $id]);
-	//$categoria->categorias=$hijos;
         $this->set(compact('categoria','nombre_padre'));
         $this->set('_serialize', ['categoria']);
     }
@@ -52,18 +50,33 @@ class CategoriasController extends AppController
     public function add()
     {
         $categoria = $this->Categorias->newEntity();
-		$categoria_padres = $this->Categorias->find('list', ['limit' => 200]);
+		$categoria_padres =$this->paginate($this->Categorias);
+		
+		$n=array();
+		foreach($categoria_padres as $v){
+			
+				array_push($n,$v->nombre);
+		}
+		
         if ($this->request->is('post')) {
-            $categoria = $this->Categorias->patchEntity($categoria, $this->request->data);
+			$nombrepadre=$n[$this->request->data['categoria_id']];
+			$this->request->data['categoria_id']=$nombrepadre;
+			$categoria = $this->Categorias->patchEntity($categoria, $this->request->data);
+			
+			foreach($categoria_padres as $v){
+				if(strcmp($v->nombre,$nombrepadre)==0){
+					$categoria->categoria_id=$v->id;
+				}
+			}
             if ($this->Categorias->save($categoria)) {
-                $this->Flash->success(__('The categoria has been saved.'));
+                $this->Flash->success(__('La categoria se ha guardado.'));
                 return $this->redirect(['action' => 'index']);
             } else {
-                $this->Flash->error(__('The categoria could not be saved. Please, try again.'));
+                $this->Flash->error(__('La categoria no se ha guardado. Pruebe otra vez.'));
             }
         }
-		
-        $this->set(compact('categoria','categoria_padres'));
+		$categoria_padres=$this->Categorias->find('list');
+        $this->set(compact('categoria','categoria_padres','n'));
         $this->set('_serialize', ['categoria']);
     }
 
@@ -79,16 +92,37 @@ class CategoriasController extends AppController
         $categoria = $this->Categorias->get($id, [
             'contain' => []
         ]);
+		$categoria_padres =$this->paginate($this->Categorias);
+		
+		$n=array("");
+		$i=1;
+		foreach($categoria_padres as $v){
+				
+				array_push($n,$v->nombre);
+				if($v->id==$categoria->categoria_id){
+					$categoria->categoria_id=$i;
+				}
+				$i=$i+1;
+		}
+		$nombrepadre="";
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $categoria = $this->Categorias->patchEntity($categoria, $this->request->data);
+            $nombrepadre=$n[$this->request->data['categoria_id']];
+			$this->request->data['categoria_id']=$nombrepadre;
+			$categoria = $this->Categorias->patchEntity($categoria, $this->request->data);
+			
+			foreach($categoria_padres as $v){
+				if(strcmp($v->nombre,$nombrepadre)==0){
+					$categoria->categoria_id=$v->id;
+				}
+			}
             if ($this->Categorias->save($categoria)) {
-                $this->Flash->success(__('The categoria has been saved.'));
+                $this->Flash->success(__('La categoria se ha guardado.'));
                 return $this->redirect(['action' => 'index']);
             } else {
-                $this->Flash->error(__('The categoria could not be saved. Please, try again.'));
+                $this->Flash->error(__('La categoria no se ha guardado. Pruebe otra vez.'));
             }
         }
-        $this->set(compact('categoria'));
+        $this->set(compact('categoria','n'));
         $this->set('_serialize', ['categoria']);
     }
 
@@ -104,6 +138,16 @@ class CategoriasController extends AppController
         $this->request->allowMethod(['post', 'delete']);
         $categoria = $this->Categorias->get($id);
         if ($this->Categorias->delete($categoria)) {
+			$categoria_hijos =$this->paginate($this->Categorias);
+			foreach($categoria_hijos as $c){
+				
+				if($id == $c->categoria_id){
+					$c->categoria_id=0;
+					 if ($this->Categorias->save($c)){
+						  return $this->redirect(['action' => 'add']);
+					 }
+				}
+			}
             $this->Flash->success(__('The categoria has been deleted.'));
         } else {
             $this->Flash->error(__('The categoria could not be deleted. Please, try again.'));
