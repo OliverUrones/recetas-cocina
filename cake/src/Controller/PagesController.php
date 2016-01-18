6c34c5e54e851db676d5f8b26c87f830b28439f7
JHaedo <jhaedo@usal.es>
2016-01-18 01:41:17 +0100

    permisos de acceso a portada

--- cake/src/Controller/PagesController.php
+++ cake/src/Controller/PagesController.php
@@ -35,6 +35,12 @@ class PagesController extends AppController
      * @throws \Cake\Network\Exception\NotFoundException When the view file could not
      *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
      */
+    public function beforeFilter(\Cake\Event\Event $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow('home');
+        $this->Auth->redirectUrl();
+    }
     public function display()
     {
         $path = func_get_args();
